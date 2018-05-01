62ff1f16ab6d6bef12113cd3ab5473c4f3e229ab
jessielute <jessie.lute@gmail.com>
2018-04-30 20:09:55 -0400

    added boilerplate code

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+//MOVIE APP
+
+//BOILERPLATE CODE
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.listen(8080, () => {
+    console.log('Server Started on http://localhost:8080');
+    console.log('Press CTRL + C to stop server');
+});
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
